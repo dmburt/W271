53c9ab3d4af2a2991e39037aa1064ba405caf527
dmburt <dmburt@berkeley.edu>
2016-12-06 01:13:57 -0500

    Updated SQL process
    Fixed residency program indicator and restored Medicare beneficiary counts by HRR.

--- Data/SQL/01 - IP Charge.sql
+++ Data/SQL/01 - IP Charge.sql
@@ -43,8 +43,10 @@ select
 	        then     c.[Average Covered Charges] / d.[RW]
 			else     0
 			end) as  [FY2014_ADJUSTED_AVG_CHG],
-	   sum(0 * c.[Average Covered Charges]) as [DIFF_AVG_CHG],                    -- Placeholders for subsequent calculation in update
-	   sum(0 * c.[Average Covered Charges]) as [DIFF_ADJUSTED_AVG_CHG],           -- Note these use existing fields to maintain float data type.
+	   sum(0 * c.[Average Covered Charges]) as [DIFF_13_12_AVG_CHG],                    -- Placeholders for subsequent calculation in update
+	   sum(0 * c.[Average Covered Charges]) as [DIFF_13_12_ADJUSTED_AVG_CHG],           -- Note these use existing fields to maintain float data type.
+   	   sum(0 * c.[Average Covered Charges]) as [DIFF_14_13_AVG_CHG],                    -- Placeholders for subsequent calculation in update
+	   sum(0 * c.[Average Covered Charges]) as [DIFF_14_13_ADJUSTED_AVG_CHG],           -- Note these use existing fields to maintain float data type.
 	   sum(0) as [N_HOSPS_IN_HRR],
 	   sum(0 * c.[Average Covered Charges]) as [HRR_FY2012_DISCHARGES_INCLUDING_HOSP],       -- Calculated average charges for HRR are calculated not
 	   sum(0 * c.[Average Covered Charges]) as [HRR_FY2012_AVG_CHG_INCLUDING_HOSP],          --  only for unadjusted and DRG relative weight-adjusted
@@ -83,23 +85,79 @@ select
 			when pos.[GNRL_CNTL_TYPE_CD] = '10' THEN 'TRIBAL'
 			else 'OTHER'
 			end as [TYPE_OF_OWNERSHIP],
-	   case when [pos].[CBSA_URBN_RRL_IND] = 'R'
+	   case when pos.[CBSA_URBN_RRL_IND] = 'R'
 	        then 'R'
 			else 'U'
 			end as [URBAN_RURAL_IND],
-	   PHYSN_SRVC_ONST_RSDNT_SW as [RESIDENCY_PROGRAM_IND],
-	   pos.[PHYSN_CNT] 
-	   + pos.[RSDNT_PHYSN_CNT] 
-       + pos.[PHYSN_OTHR_CNTRCT_CNT]
-       + pos.[PHYSN_OTHR_FLTM_CNT]
-       + pos.[PHYSN_OTHR_PRTM_CNT] as [N_PHYSICIANS],
-       pos.[RN_FLTM_CNT]
-       + pos.[RN_CNTRCT_CNT]
-       + pos.[RN_PRTM_CNT]
-       + pos.[LPN_LVN_FLTM_CNT] 
-       + pos.[CRNA_CNT]
-       + pos.[NRS_PRCTNR_CNT] as [N_NURSES]
-			
+	   
+	   CASE WHEN (
+	       CASE WHEN pos.[RSDNT_PGM_ALPTHC_SW] = 'Y' 
+	            THEN 1
+			    ELSE 0
+			    END
+		   + CASE WHEN pos.[RSDNT_PGM_DNTL_SW] = 'Y' 
+	            THEN 1
+			    ELSE 0
+			    END
+		   + CASE WHEN pos.[RSDNT_PGM_OSTPTHC_SW] = 'Y' 
+	            THEN 1
+			    ELSE 0
+			    END
+		   + CASE WHEN pos.[RSDNT_PGM_OTHR_SW] = 'Y' 
+	            THEN 1
+			    ELSE 0
+			    END
+		   + CASE WHEN pos.[RSDNT_PGM_PDTRC_SW] = 'Y' 
+	            THEN 1
+			    ELSE 0
+			    END
+		   ) > 0 THEN 'Y' ELSE 'N' END as [RESIDENCY_PROGRAM_IND],
+	   case when isnumeric(pos.[PHYSN_CNT]) = 0
+            then 0
+            else cast(pos.[PHYSN_CNT] as float)
+      	    end
+       + case when isnumeric(pos.[RSDNT_PHYSN_CNT]) = 0
+            then 0
+	        else cast(pos.[RSDNT_PHYSN_CNT] as float)
+	        end
+       + case when isnumeric(pos.[PHYSN_OTHR_CNTRCT_CNT]) = 0
+            then 0
+	        else cast(pos.[PHYSN_OTHR_CNTRCT_CNT] as float)
+	        end
+       + case when isnumeric(pos.[PHYSN_OTHR_FLTM_CNT]) = 0
+            then 0
+	        else cast(pos.[PHYSN_OTHR_FLTM_CNT] as float)
+	        end
+       + case when isnumeric(pos.[PHYSN_OTHR_PRTM_CNT]) = 0
+            then 0
+	        else cast(pos.[PHYSN_OTHR_PRTM_CNT] as float)
+	        end as [N_PHYSICIANS],
+
+      case when isnumeric(pos.[RN_FLTM_CNT]) = 0
+           then 0
+	       else cast(pos.[RN_FLTM_CNT] as float)
+	       end
+      + case when isnumeric(pos.[RN_CNTRCT_CNT]) = 0
+           then 0
+      	   else cast(pos.[RN_CNTRCT_CNT] as float)
+      	   end
+      + case when isnumeric(pos.[RN_PRTM_CNT]) = 0
+          then 0
+      	  else cast(pos.[RN_PRTM_CNT] as float)
+      	  end
+      + case when isnumeric(pos.[LPN_LVN_FLTM_CNT]) = 0 
+          then 0
+      	  else cast(pos.[LPN_LVN_FLTM_CNT] as float)
+      	  end
+      + case when isnumeric(pos.[CRNA_CNT]) = 0
+          then 0
+      	  else cast(pos.[CRNA_CNT] as float)
+      	  end
+      + case when isnumeric(pos.[NRS_PRCTNR_CNT]) = 0
+          then 0
+      	  else cast(pos.[NRS_PRCTNR_CNT] as float)
+      	  end as [N_NURSES],
+	  e.PART_A_BENEFICIARIES
 
 INTO W271_IP_CHARGE
 
@@ -109,14 +167,21 @@ join dbo.DRG d
   on left(c.[DRG Definition], 3) = d.[DRG]
  and c.[Year] = d.[Year]
 
-join dbo.Provider_of_Services_File pos
+join [Provider_of_Services_File_-_OTHER_-_December_2014] pos
   on c.[Provider Id] = pos.[PRVDR_NUM]
+
+join dbo.Medicare_Enrollment_By_HRR e
+  on c.[Hospital Referral Region (HRR) Description] = e.HRR_NAME
 
 where c.[Year] between 2012 and 2014
   and c.[Provider Id] <> '000000'
+  --and c.[Provider Id] = '010001'
+  and pos.prvdr_ctgry_cd = '01'
+  and e.[YEAR] = 2014
+
 --  and c.[Provider Id] = '010001'
 --  and left(c.[DRG Definition], 3) in ('039','057')
-  and pos.[YEAR] = 2014
+  --and pos.[YEAR] = 2014
 
 group by c.[Provider Id],
          c.[Hospital Referral Region (HRR) Description],
@@ -143,20 +208,26 @@ group by c.[Provider Id],
 			when GNRL_CNTL_TYPE_CD = '10' THEN 'TRIBAL'
 			else 'OTHER'
 			end,
-		 case when [pos].[CBSA_URBN_RRL_IND] = 'R'
+		 case when pos.[CBSA_URBN_RRL_IND] = 'R'
 	        then 'R'
 			else 'U'
 			end,
-	   PHYSN_SRVC_ONST_RSDNT_SW,
-	   pos.[PHYSN_CNT] 
-	   + pos.[RSDNT_PHYSN_CNT] 
-       + pos.[PHYSN_OTHR_CNTRCT_CNT]
-       + pos.[PHYSN_OTHR_FLTM_CNT]
-       + pos.[PHYSN_OTHR_PRTM_CNT],
-       pos.[RN_FLTM_CNT]
-       + pos.[RN_CNTRCT_CNT]
-       + pos.[RN_PRTM_CNT]
-       + pos.[LPN_LVN_FLTM_CNT] 
-       + pos.[CRNA_CNT]
-       + pos.[NRS_PRCTNR_CNT]
+       pos.[RSDNT_PGM_ALPTHC_SW],
+       pos.[RSDNT_PGM_DNTL_SW],
+       pos.[RSDNT_PGM_OSTPTHC_SW],
+       pos.[RSDNT_PGM_OTHR_SW],
+       pos.[RSDNT_PGM_PDTRC_SW],
+	   pos.[PHYSN_CNT],
+	   pos.[RSDNT_PHYSN_CNT],
+       pos.[PHYSN_OTHR_CNTRCT_CNT],
+       pos.[PHYSN_OTHR_FLTM_CNT],
+       pos.[PHYSN_OTHR_PRTM_CNT],
+       pos.[RN_FLTM_CNT],
+       pos.[RN_CNTRCT_CNT],
+       pos.[RN_PRTM_CNT],
+       pos.[LPN_LVN_FLTM_CNT],
+       pos.[CRNA_CNT],
+       pos.[NRS_PRCTNR_CNT],
+	   e.PART_A_BENEFICIARIES
 
+order by 3
