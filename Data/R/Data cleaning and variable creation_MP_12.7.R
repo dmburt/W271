28d9bbdc22564a692ea89bf8d1e4317cb40955e2
Steve Pelkey <spelkey@ucdavis.edu>
2016-12-07 18:03:26 -0800

    added my working directory in addition to yours

--- Data/R/Data cleaning and variable creation_MP_12.7.R
+++ Data/R/Data cleaning and variable creation_MP_12.7.R
@@ -2,8 +2,11 @@
 # The output is a dataframe that will be used for the enirity of analysis
 
 # loading data and saving to a dataframe
-setwd("~/Desktop/Cal/MIDS/271/271 final project/")
-d <- read.delim("W271_Lab3_Dataset_By_Hosp.txt")
+#setwd("~/Desktop/Cal/MIDS/271/271 final project/")
+#d <- read.delim("W271_Lab3_Dataset_By_Hosp.txt")
+
+setwd("c:/Users/Steve/Documents/mids/w_271/project/W271/Data/R/")
+d <- read.delim("../../../W271_Lab3_Dataset_By_Hosp.txt")
 
 # Excluding hospitals that have no charges in either 2013 or 2014 (or both)
 d <- d[ which(d$FY2013_AVG_CHG!=0 & d$FY2014_AVG_CHG!=0), ]
@@ -24,3 +27,7 @@ d$BED_CNT_PER_THOUSAND_BENEFICIARIES = d$BED_CNT/d$BENEFICIARIES_PER_PROVIDER
 
 # Checking for missing values
 sapply(d, function(x) sum(is.na(x))) # only missing values are in bed count; how do we want to deal with these?
+d = d[complete.cases(d),]
+
+# big outliers, remove?
+#d = d[which(d$DIFF_14_13_ADJUSTED_AVG_CHG < 79000 & d$DIFF_14_13_ADJUSTED_AVG_CHG > -33000),]
